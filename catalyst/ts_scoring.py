054f859e3df4884fc29e00ba7b8940759f5724cf
juius <hwg245@alumni.ku.dk>
2021-03-02 15:02:29 +0100

    add warning_logger

--- catalyst/ts_scoring.py
+++ catalyst/ts_scoring.py
@@ -3,13 +3,14 @@ import numpy as np
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+import logging
 import copy
 import shutil
 import os # this is jsut vor OMP
 import sys
 sys.path.append('/home/julius/soft/GB-GA/')
 from catalyst.utils import sdf2mol, draw3d, mol_from_xyz, vis_trajectory, Timer
-from catalyst.make_structures import ConstrainedEmbedMultipleConfsMultipleFrags, connect_cat_2d, check_num_frags
+from catalyst.make_structures import ConstrainedEmbedMultipleConfsMultipleFrags, connect_cat_2d
 from catalyst.xtb_utils import xtb_optimize, write_xtb_input_files 
 ts_dummy = sdf2mol('/home/julius/soft/GB-GA/catalyst/structures/ts_dummy.sdf')
 frag_energies = np.sum([-8.232710038092, -19.734652802142, -32.543971411432]) # 34 atoms
@@ -20,12 +21,12 @@ frag_energies = np.sum([-8.232710038092, -19.734652802142, -32.543971411432]) # 
 
 # %%
 def ts_scoring(mol, args_list): # to be used in calculate_scores_parallel(population,final_scoring,[gen_num, n_confs, randomseed],n_cpus)
-    ind_num, gen_num, n_confs, randomseed, logger, directory, n_cpus = args_list
-    energy = activation_barrier(cat=mol, gen_num=gen_num, ind_num=ind_num, n_confs=n_confs, randomseed=randomseed, numThreads=n_cpus, logger=logger, directory=directory) 
+    ind_num, gen_num, n_confs, randomseed, timing_logger, warning_logger, directory, n_cpus = args_list
+    energy = activation_barrier(cat=mol, gen_num=gen_num, ind_num=ind_num, n_confs=n_confs, randomseed=randomseed, numThreads=n_cpus, timing_logger=timing_logger, warning_logger=warning_logger, directory=directory) 
     return energy
 
-def activation_barrier(cat, ind_num, gen_num, n_confs=5, randomseed=101, numThreads=1, logger=None, directory='.'):
-    if logger:
+def activation_barrier(cat, ind_num, gen_num, n_confs=5, randomseed=101, numThreads=1, timing_logger=None, warning_logger=None, directory='.'):
+    if timing_logger:
         t1 = Timer(logger=None)
         t1.start()
     ts2ds = connect_cat_2d(ts_dummy, cat)
@@ -51,11 +52,14 @@ def activation_barrier(cat, ind_num, gen_num, n_confs=5, randomseed=101, numThre
                 force_constant = force_constant/2
             tries += 1
             if tries == max_tries:
-                print(f'Embedding of {Chem.MolToSmiles(ts2d)} was not successful in {max_tries} tries with final forceconstant={force_constant}.')
+                if warning_logger:
+                    warning_logger.warning(f'Embedding of {Chem.MolToSmiles(ts2d)} was not successful in {max_tries} tries with final forceconstant={force_constant}')
+                else:
+                    print(f'Embedding of {Chem.MolToSmiles(ts2d)} was not successful in {max_tries} tries with final forceconstant={force_constant}')
                 break
                 # raise Exception(f'Embedding of {Chem.MolToSmiles(ts2d)} was not successful in {max_tries} tries with final forceconstant={force_constant}.')
         # xTB optimite TS
-        ts3d_file, ts3d_energy = xtb_optimize(ts3d, name=f'G{gen_num:02d}_I{ind_num:02d}/const_iso{i:03d}', constrains='/home/julius/thesis/data/constr.inp', scratchdir=directory, remove_tmp=False, return_file=True, numThreads=numThreads)
+        ts3d_file, ts3d_energy = xtb_optimize(ts3d, name=f'G{gen_num:02d}_I{ind_num:02d}/const_iso{i:03d}', constrains='/home/julius/thesis/data/constr.inp', scratchdir=directory, remove_tmp=False, return_file=True, numThreads=numThreads, warning_logger=warning_logger)
         # here should be a num frag check
         ts3d_energies.append(ts3d_energy)
         ts3d_files.append(ts3d_file)
@@ -64,21 +68,21 @@ def activation_barrier(cat, ind_num, gen_num, n_confs=5, randomseed=101, numThre
         min_e_index = ts3d_energies.index(ts_energy)
         ts_file = ts3d_files[min_e_index]
     except:
-        print(f'xTB optimization led to missmatch in number of fragments for {Chem.MolToSmiles(ts2d)}')
-        print(-100000)
+        if warning_logger:
+            warning_logger.warning(f'xTB optimization led to missmatch in number of fragments for {Chem.MolToSmiles(ts2d)}')
+        else:
+            print(f'xTB optimization led to missmatch in number of fragments for {Chem.MolToSmiles(ts2d)}')
+        return(-100000)
     cat_path = os.path.join(directory, f'G{gen_num:02d}_I{ind_num:02d}/const_iso{i:03d}/catalyst')
     os.mkdir(cat_path)
     cat_file = isolate_cat_from_xyz(ts_file, os.path.join(cat_path, 'cat.xyz'))
-    cat_opt_file, cat_energy = xtb_optimize(cat_file, name=None, scratchdir=directory, remove_tmp=False,return_file=True, numThreads=numThreads)
-    if logger:
+    cat_opt_file, cat_energy = xtb_optimize(cat_file, name=None, scratchdir=directory, remove_tmp=False,return_file=True, numThreads=numThreads, warning_logger=warning_logger)
+    if timing_logger:
         elapsed_time = t1.stop()
-        logger.info(f'{Chem.MolToSmiles(cat)} : {elapsed_time:0.4f} seconds')
+        timing_logger.info(f'{Chem.MolToSmiles(cat)} : {elapsed_time:0.4f} seconds')
     return ts_energy-cat_energy-frag_energies
 
 def isolate_cat(mol, scarfold):
-    if not mol.HasSubstructMatch(scarfold):
-        print(f'Optimization of {Chem.MolToSmiles(mol)} did not go right')
-        return None
     cat = AllChem.ReplaceCore(mol, scarfold, replaceDummies=False)
     for atom in cat.GetAtoms():
         if atom.GetAtomicNum() == 0:

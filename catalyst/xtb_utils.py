52594dc4b020931192f05ed4f476ca8e223c6a07
juius <hwg245@alumni.ku.dk>
2021-03-02 19:10:01 +0100

    fix return_file path

--- catalyst/xtb_utils.py
+++ catalyst/xtb_utils.py
@@ -106,7 +106,7 @@ def xtb_optimize(mol, name=None, constrains=None, charge=None, method='gfn2', so
     if remove_tmp:
         shutil.rmtree(dest)
     if return_file:
-        return os.path.abspath(min_e_file), energy
+        return os.path.join(dest, 'min_e_conf.xyz'), energy
     else:
         atoms, _, coordinates = read_xyz_file(min_e_file)
         # takes charge as defined before optimization
@@ -123,74 +123,4 @@ def run_xtb_path(reactant_file, product_file, inp_file='/home/julius/thesis/data
     ts_energy = get_energy_from_xtb_sp(os.path.join(path_dir, 'xtbpath_ts.xyz'))
     return ts_energy
 
-
-#         def xtb_optimize_conformers(args):
-#     xyz_file, i, dest, constrains, solvent, numThreads_per_conf, method, opt_level = args
-#     org_dir = os.getcwd()
-#     conf_path = os.path.join(dest, f'conf{i:03d}')
-#     print(conf_path)
-#     os.chdir(conf_path)
-#     print(os.getcwd())
-#     if constrains:
-#         constrains_input = f'--input {constrains}'
-#     else:
-#         constrains_input = ''
-#     if solvent:
-#         solvent_input = f'--{solvent}'
-#     else:
-#         solvent_input = ''
-#     os.environ['OMP_NUM_THREADS'] = f'{numThreads_per_conf},1'
-#     os.environ['MKL_NUM_THREADS'] = f'{numThreads_per_conf}'
-#     os.environ['OMP_STACKSIZE'] = '4G'
-#     p = subprocess.Popen(
-#         f'/home/julius/soft/xtb-6.3.3/bin/xtb --{method} {xyz_file} --opt {opt_level} {constrains_input} {solvent_input} --json > out.out', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#     output, err = p.communicate()
-#     out_file = 'xtbopt.xyz'
-#     try:
-#         energy = get_energy_from_xtb_sp(out_file)
-#     except:
-#         energy = 1000
-#     os.chdir(org_dir)
-#     return energy, os.path.abspath(out_file)
-
-# def xtb_optimize(mol, name=None, constrains=None, method='gfn2', solvent='alpb methanol', opt_level='tight', scratchdir='/home/julius/thesis/sims/scratch', remove_tmp=True, numThreads=1):
-#     if mol.GetNumAtoms(onlyExplicit=True) < mol.GetNumAtoms(onlyExplicit=False):
-#         raise Exception('Implicit Hydrogens')
-#     conformers = mol.GetConformers()
-#     if not conformers:
-#         raise Exception('Mol is not embedded')
-#     elif not conformers[-1].Is3D():
-#         raise Exception('Conformer is not 3D')
-#     true_charge = Chem.GetFormalCharge(mol)
-#     if not name:
-#         name = 'tmp_' + ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
-#     dest = os.path.join(
-#         os.path.abspath(scratchdir), name)
-#     os.makedirs(dest)
-#     energies = []
-#     xyz_files = write_xtb_input_files(mol, 'xtbmol', destination=dest)
-#     numThreads_per_conf = int(numThreads/len(xyz_files))
-#     args_list = []
-#     for i, xyz_file in enumerate(xyz_files):
-#         args_list.append([xyz_file, i, dest, constrains, solvent, numThreads_per_conf, method, opt_level])
-#     print(args_list)
-#     with Pool(numThreads) as pool:
-#         output = pool.map(xtb_optimize_conformers, args_list)
-#     print(output)
-#     out = np.array(output)
-#     energies = list(out[:,0])
-#     out_files = list(out[:,1])
-#     print(energies)
-#     print(out_files)
-#     min_e_index = energies.index(min(energies))
-#     min_e_file = out_files[min_e_index]
-#     shutil.copy(min_e_file, os.path.join(dest, 'min_e_conf.xyz'))
-#     atoms, charge, coordinates = read_xyz_file(min_e_file)
-#     # takes charge as defined before optimization
-#     new_mol = xyz2mol(atoms, coordinates, true_charge)
-#     if remove_tmp:
-#         shutil.rmtree(dest)
-#     if return_file:
-#         return os.abspath(min_e_file), energy
-#     else:
-#         return new_mol, energy
+# %%

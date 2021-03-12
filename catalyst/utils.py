2b7657b586dabf463d97f3bdf21a85b421292b97
juius <hwg245@alumni.ku.dk>
2021-03-12 16:44:35 +0100

    add parents

--- catalyst/utils.py
+++ catalyst/utils.py
@@ -135,28 +135,51 @@ from tabulate import tabulate
 import copy
 import pandas as pd
 
+
+
+
+# # %%
+# import matplotlib.pyplot as plt
+
+# test = np.linspace(-4,4)
+# out = []
+# for val in test:
+#     out.append(scaling_function(val, method='other'))
+# plt.plot(test,out)
+
+# %%
+# %%
+
 @dataclass
 class Individual:
     rdkit_mol:          Chem.rdchem.Mol = field(repr=False, compare=False)
-    smiles:             str = field(init=False, compare=True)
+    smiles:             str = field(init=False, compare=True, repr=True)
     idx:                tuple = field(default=(None,None), repr=False, compare=False)
+    parentA_idx:        tuple = field(default=None, repr=False, compare=False)
+    parentB_idx:        tuple = field(default=None, repr=False, compare=False)
+    survival_idx:       tuple = field(default=None, repr=False, compare=False)
     score:              float = field(default=None, repr=False, compare=False)
     energy:             float = field(default=None, repr=False, compare=False)
     sa_score:           float = field(default=None, repr=False, compare=False)
     normalized_fitness: float = field(default=None, repr=False, compare=False)
     neutral_rdkit_mol:  Chem.rdchem.Mol = field(init=False, repr=False, compare=False)
     warnings:           List[str] = field(default_factory=list, repr=False, compare=False) 
-    # parents:            
 
     def __post_init__(self):
         self.smiles = Chem.MolToSmiles(self.rdkit_mol)
+        num_parents = 0
+        if self.parentA_idx:
+            num_parents += 1
+        if self.parentB_idx:
+            num_parents += 1
+        self.num_parents = num_parents
 
-    def update(self):
-        if self.energy and self.sa_score:
-            setattr(self, 'score', scaling_function(self.energy)*self.sa_score)
+    # def update(self, method='linear', from_min=-3, from_max=1, a=2, b=-2):
+    #     if self.energy and self.sa_score:
+    #         setattr(self, 'score', scaling_function(self.energy, method=method, from_min=from_min, from_max=from_max, a=a, b=b)*self.sa_score)
 
     def list_of_props(self):
-        return([self.idx[1], self.score, self.energy, self.sa_score, self.smiles])
+        return([self.idx[1], self.normalized_fitness, self.score, self.energy, self.sa_score, self.smiles])
 
 @dataclass(order=True)
 class Population:
@@ -172,9 +195,9 @@ class Population:
             setattr(molecule, 'idx', (self.generation_num, i))
         self.size = len(self.molecules)
     
-    def update(self):
-        for molecule in self.molecules:
-            molecule.update()
+    # def update(self):
+    #     for molecule in self.molecules:
+    #         molecule.update()
 
     def get(self, prop):
         properties = []
@@ -192,7 +215,10 @@ class Population:
                 getattr(molecule, prop).append(value)
 
     def sortby(self, prop, reverse=True):
-        self.molecules.sort(key=lambda x: getattr(x,prop), reverse=reverse)
+        if reverse:
+            self.molecules.sort(key=lambda x: (getattr(x,prop) is not None, getattr(x,prop)), reverse=reverse)
+        else:
+            self.molecules.sort(key=lambda x: (getattr(x,prop) is None, getattr(x,prop)), reverse=reverse)
 
     def prune(self, population_size):
         self.sortby('score', reverse=True)
@@ -205,7 +231,7 @@ class Population:
             table.append(individual.list_of_props())
         if isinstance(self.generation_num, int):
             print(f'\nGeneration {self.generation_num:02d}')
-        print(tabulate(table, headers=['idx', 'score', 'energy', 'sa_score', 'smiles']))
+        print(tabulate(table, headers=['idx', 'normalized_fitness', 'score', 'energy', 'sa_score', 'smiles']))
 
     def pop2pd(self, columns = ['score', 'energy', 'sa_score', 'rdkit_mol']):
         df = pd.DataFrame(list(map(list, zip(*[self.get(prop) for prop in columns]))), index=pd.MultiIndex.from_tuples(self.get('idx'), names=('generation', 'individual')))
@@ -214,9 +240,10 @@ class Population:
 
 @dataclass(order=True)
 class Generation:
-    generation_num:     int = field(init=True) # check that its same as in both populations without reindexing individuals in populations
+    generation_num:     int = field(init=True) 
     children:           Population= field(default_factory=Population) 
     survivors:          Population = field(default_factory=Population)
+    # a counter to count how ofter molecules got flagged during mutation/crossover by filter
 
     def __post_init__(self):
         if self.generation_num != self.children.generation_num or self.generation_num != self.survivors.generation_num:
@@ -238,16 +265,16 @@ class Generation:
         for individual in population.molecules:
             table.append(individual.list_of_props())
         print(f'\nGeneration {self.generation_num:02d}')
-        print(tabulate(table, headers=['idx', 'score', 'energy', 'sa_score', 'smiles']))
+        print(tabulate(table, headers=['idx', 'normalized_fitness', 'score', 'energy', 'sa_score', 'smiles']))
     
-    def gen2pd(self, population='survivors'):
+    def gen2pd(self, population='survivors', columns = ['score', 'energy', 'sa_score', 'rdkit_mol']):
         if population == 'survivors':
             population = self.survivors
         elif population == 'children':
             population = self.children
         else:
             raise TypeError(f'{population} is not a valid option (survivors/children)')
-        pd = population.pop2pd()
+        pd = population.pop2pd(columns=columns)
         return pd
 
     def __repr__(self):
@@ -266,13 +293,71 @@ class GA_run:
         self.generations += [generation]
         self.num_generations = len(self.generations)
 
+    def get_ind(self, idx):
+        return self.generations[idx[0]].survivors.molecules[idx[1]]
+
+    def get_parent_tuple(self, individual):
+        if not individual.parentA_idx and not individual.parentB_idx:
+            return None
+        if individual.parentA_idx:
+            parentA = self.get_ind(individual.parentA_idx)
+        if individual.parentB_idx:
+            parentB = self.get_ind(individual.parentB_idx)
+        return tuple((individual.idx, parentA.idx, parentB.idx))
+
+    def get_survivor(self, individual):
+        if not individual.survival_idx:
+            return None
+        else:
+            return tuple((individual.idx, individual.survival_idx))
+
+    def get_origin(self, individual):
+        if individual.survival_idx:
+            return self.get_survivor(individual)
+        elif individual.parentA_idx:
+            return self.get_parent_tuple(individual)
+
+    def traceback(self, idx, max_its=100):
+        trace = []
+        checked = []
+        youngest = self.get_ind(idx)
+        trace.append(self.get_origin(youngest))
+        counter = 0
+        for node in trace:
+            if not node:
+                break
+            iD = node[0]
+            if iD in checked:
+                continue # with next node
+            trace.append(self.get_origin(self.get_ind(node[1])))
+            if len(node) == 3:
+                trace.append(self.get_origin(self.get_ind(node[2])))
+            counter += 1
+            if counter > max_its:
+                break
+        return [x for x in trace if x is not None]
+
+                
+
+
     def print(self, population='survivors'):
         for generation in self.generations:
             generation.print(population)
 
-    def ga2pd(self, population='survivors'):
-        df = pd.concat([generation.gen2pd(population) for generation in self.generations])
+    def ga2pd(self, population='survivors', columns = ['score', 'energy', 'sa_score', 'rdkit_mol']):
+        df = pd.concat([generation.gen2pd(population, columns=columns) for generation in self.generations])
         return df
+
+def load_GA(pkl):
+    with open(pkl, "rb") as rfp:
+        ga = GA_run()
+        while True:
+            try:
+                generation = pickle.load(rfp)
+                ga.append_gen(generation)
+            except EOFError:
+                break
+    return ga
 # %%
 
 if __name__ == '__main__':
@@ -280,11 +365,26 @@ if __name__ == '__main__':
     sys.path.append('/home/julius/soft/GB-GA/')
     import GB_GA as ga 
 
-    # %%
-    population1 = ga.make_initial_population(5, '/home/julius/thesis/data/ZINC_1000_amines.smi')
-    population2 = ga.make_initial_population(5, '/home/julius/thesis/data/ZINC_1000_amines.smi')
+    def load_GA(pkl):
+        with open(pkl, "rb") as rfp:
+            ga = GA_run()
+            while True:
+                try:
+                    generation = pickle.load(rfp)
+                    ga.append_gen(generation)
+                except EOFError:
+                    break
+        return ga
 
-    gen = Generation(generation_num=0, children=population1, survivors=population2)
 # %%
+    ga = load_GA('/home/julius/thesis/data/ZINC_scoring/ts_scoring/GA_output.pkl')
+    
+# %%
+    ind1 = Individual(Chem.MolFromSmiles('CCC'), energy=100, score=1)
+    ind2 = Individual(Chem.MolFromSmiles('CNC'), energy=None, score=None)
+    ind3 = Individual(Chem.MolFromSmiles('COC'), energy=-2, score=10)
+
+    pop = Population(generation_num=0, molecules=[ind1,ind2,ind3])
+    pop.assign_idx()
 
 # %%

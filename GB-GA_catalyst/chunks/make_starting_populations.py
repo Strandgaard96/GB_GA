d5b7e0274fd51fd74ab550babd60686fe3b9f43f
juius <hwg245@alumni.ku.dk>
2021-03-22 11:19:05 +0100

    initial commit

---
+++ GB-GA_catalyst/chunks/make_starting_populations.py
@@ -0,0 +1,19 @@
+# %%
+import sys
+sys.path.append('/home/julius/soft/GB-GA')
+
+from catalyst.utils import mols_from_smi_file
+
+# %%
+with open('/home/julius/soft/GB-GA/ZINC_1000_amines.smi', 'r') as f:
+    o = None
+    counter = 0
+    for i, line in enumerate(f):
+        if i%50 == 0:
+            if o:
+                o.close()
+            o = open(f'/home/julius/soft/GB-GA/GB-GA_catalyst/chunks/pop{counter:02d}.smi', 'w')
+            counter +=1
+        o.write(line)
+    if o:
+        o.close()

fc0a901e96ca0363437156b8c502e5c9cc77d1e2
Strandgaard96 <magnus.str@gmail.com>
2022-02-08 09:41:01 +0100

    added submit script

--- test/xtb_calc_runner.py
+++ test/xtb_calc_runner.py
@@ -1,13 +1,18 @@
 import os
 import sys
 from pathlib import Path
+sys.path.append(
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            "..")))
 
 from my_utils import auto, my_utils, my_xtb_utils
 
 if __name__ == '__main__':
 
     path = Path(
-        "/home/magstr/Documents/nitrogenase/schrock/cycle")
+        "/home/energy/magstr/nitrogenase/xtb_optimization_pureXTB_6.3.3/bases/schrock/part1")
 
     spin = True
     struct = ".xyz"
@@ -31,10 +36,10 @@ if __name__ == '__main__':
             my_xtb_utils.run_xtb(
                 structure=elem.split('/')[-1],
                 method='gfn2',
-                type="scc",
+                type="ohess",
                 charge=charge,
                 spin=spin,
                 gbsa='Benzene')
 
-            if elem == paths[0]:
+            if elem == paths[-1]:
                 sys.exit(0)

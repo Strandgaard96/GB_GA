0680a7c6d52ecc6b9f7d879b693ba4b124766935
Strandgaard96 <magnus.str@gmail.com>
2022-02-23 13:58:38 +0100

    Added small argparse example. Currently not in use. Also, fixed imports
    Added some more default arguments and core with hydrogen to templates.
    
    Added some more default arguments.

--- test/molSimplify_driver.py
+++ test/molSimplify_driver.py
@@ -1,10 +1,54 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from my_utils.my_utils import cd
 from my_utils.my_xtb_utils import run_xtb
 from my_utils.auto import shell, get_paths_custom
 from pathlib import Path
 import json
 import shutil
-import os
+import argparse
+
+def get_arguments(arg_list=None):
+    """
+    Args:
+        arg_list: Automatically obtained from the commandline if provided. Otherwise default arguments are used
+
+    Returns:
+        parser.parse_args(arg_list)(Namespace): Dictionary like class that contain the arguments
+
+    """
+    parser = argparse.ArgumentParser(
+        description="Run GA algorithm", fromfile_prefix_chars="+"
+    )
+    parser.add_argument(
+        "--run_dir",
+        type=str,
+        default="Runs",
+        help="Sets the output dir of the molSimplify commands",
+    )
+    parser.add_argument(
+        "--cycle_dir",
+        type=str,
+        default="Runs_cycle",
+        help="Sets the output dir of the cycle molSimplify commands",
+    )
+    parser.add_argument(
+        "--cleanup",
+        dest='cleanup',
+        action='store_true',
+        help="Cleans xtb output folders",
+    )
+    parser.set_defaults(cleanup=True)
+    parser.add_argument(
+        "--ligand_smi",
+        type=str,
+        default='[CH4]',
+        help="Set the ligand to put on the core",
+    )
+    return parser.parse_args(arg_list)
+
 
 
 def create_cycleMS(new_core=None, smi_path=None, run_dir=None):
@@ -77,20 +121,32 @@ def xtb_calc(run_dir=None, param_path=None):
             if elem == paths[-1]:
                 sys.exit(0)
 
+def cleanup(xtbout=None):
+    '''
+    Function to clean up file paths and make the xtb results
+    easierly accesible
+    Args:
+        xtbout (str): path to the dir where xtb calcs were put
+    Returns:
+        None
+    '''
+    return
 
 def main():
 
+    args = get_arguments()
+
     # What ligand do i want to test?
-    lig_smi = "C1=CC=CC=C1"
+    lig_smi = args.ligand_smi
     core_file = Path("../templates/core_withHS.xyz")
     replig = 1
     suffix = "core_custom"
-    run_dir = Path("Runs")
+    run_dir = Path(args.run_dir)
 
     core_cmd = (
         f"molsimplify -core {core_file} -lig {lig_smi} -replig 1 -ligocc 3"
         f" -ccatoms 24, 25, 26 -skipANN True -spin 1 -oxstate 3 -ffoption no"
-        f" -coord 5 -keepHs no -smicat 1"
+        f" -coord 5 -keepHs no -smicat 1 -rundir {run_dir}"
     )
     print(f"String passed to shell: {core_cmd}")
     out, err = shell(
@@ -106,20 +162,21 @@ def main():
     new_core = sorted(run_dir.glob("**/*.xyz"))
     intermediate_smi_path = Path("../data/intermediate_smiles.json")
 
-    # Move output structure to current dir
-    shutil.copy(new_core[0], "./new_core.xyz")
+    print(new_core)
 
     if new_core:
-        print("lol")
+        # Move output structure to current dir
+        shutil.copy(new_core[0], "./new_core.xyz")
         # Pass the output structure to cycle creation
         create_cycleMS(
             new_core="new_core.xyz",
             smi_path=intermediate_smi_path,
-            run_dir="Runs_cycle",
+            run_dir=args.cycle_dir,
         )
 
-    xtb_calc(run_dir="Runs_cycle", param_path=intermediate_smi_path)
-
+    xtb_calc(run_dir=args.cycle_dir, param_path=intermediate_smi_path)
+    if args.cleanup:
+        print('Do something')
 
 if __name__ == "__main__":
     main()

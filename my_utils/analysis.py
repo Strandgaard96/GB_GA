dd117ca43bff976f620081d631c2e1a73ab4076d
Strandgaard96 <magnus.str@gmail.com>
2022-05-05 11:13:37 +0200

    - Added analysis script to put reusable GA_analysis functions in. - Changed to AND NOT amide condition in tert amine check.

---
+++ my_utils/analysis.py
@@ -0,0 +1,25 @@
+# improts
+import sys
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+from pathlib import Path
+from glob import glob
+import pickle
+import shutil
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw import IPythonConsole, MolsToGridImage
+import py3Dmol
+
+
+
+
+def main():
+    return
+
+
+
+if __name__ == '__main__':
+    main()
